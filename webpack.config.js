10d682db85796047c432d651617e9410806e6287
kentaro <100641287+kentaro1234@users.noreply.github.com>
2022-03-06 19:29:44 +0900

    画像の最適化

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,8 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
+  devtool: 'source-map',
   entry: './src/javascripts/main.js',
   output: {
     path: path.resolve(__dirname, './dist'),
@@ -15,13 +17,30 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.js/,
+        exclude: /node__modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                ['@babel/preset-env', { 'targets': '> 0.25%, not dead' }]
+              ]
+            }
+          }
+        ]
+      },
+      {
         test: /\.(css|sass|scss)/,
         use: [
           {
             loader: MiniCssExtractPlugin.loader
           },
           {
-            loader: 'css-loader'
+            loader: 'css-loader',
+            options: {
+              sourceMap: false
+            }
           },
           {
             loader: 'sass-loader'
@@ -29,12 +48,22 @@ module.exports = {
         ]
       },
       {
-        test: /\.(png|jpg)/,
+        test: /\.(png|jpg|jpeg)/,
         type: 'asset/resource',
         generator: {
           filename: 'images/[name][ext]'
         },
-        use: []
+        use: [
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 65
+              }
+            }
+          }
+        ]
       },
       {
         test: /\.pug/,
